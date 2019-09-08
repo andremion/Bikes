3b5ff916642d0f5327b2a1e820b4e91fda251782
André Mion <andremion@gmail.com>
2019-09-08 15:07:47 +0100

    Simulate network latency in data source not in repository

--- data/src/main/java/com/andremion/bikes/data/BikesRepository.kt
+++ data/src/main/java/com/andremion/bikes/data/BikesRepository.kt
@@ -6,18 +6,13 @@ import com.andremion.bikes.data.local.BikesLocalDataSource
 import com.andremion.bikes.data.remote.BikesRemoteDataSource
 import com.andremion.bikes.data.remote.model.LocationRemote
 import com.andremion.bikes.data.remote.model.NetworkRemote
-import kotlinx.coroutines.delay
 
 class BikesRepository(
     private val remoteDataSource: BikesRemoteDataSource,
     private val localDataSource: BikesLocalDataSource
 ) {
 
-    suspend fun findNetworks(): List<Network> {
-        // TODO Temporary simulation of network latency
-        delay(2000)
-        return remoteDataSource.findNetworks().toEntityList()
-    }
+    suspend fun findNetworks(): List<Network> = remoteDataSource.findNetworks().toEntityList()
 
     suspend fun getNetworkById(id: String) = remoteDataSource.getNetworkById(id).toEntity()
 }
--- data/src/main/java/com/andremion/bikes/data/remote/BikesRemoteDataSource.kt
+++ data/src/main/java/com/andremion/bikes/data/remote/BikesRemoteDataSource.kt
@@ -1,12 +1,18 @@
 package com.andremion.bikes.data.remote
 
 import com.andremion.bikes.data.remote.api.CityBikeService
+import com.andremion.bikes.data.remote.model.NetworkRemote
+import kotlinx.coroutines.delay
 
 class BikesRemoteDataSource(
     private val service: CityBikeService
 ) {
 
-    suspend fun findNetworks() = service.findNetworks().networks
+    suspend fun findNetworks(): List<NetworkRemote> {
+        // TODO Temporary simulation of network latency
+        delay(2000)
+        return service.findNetworks().networks
+    }
 
     suspend fun getNetworkById(id: String) = service.getNetworkById(id).network
 }
